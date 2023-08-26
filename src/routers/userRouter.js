e309d69473ea569992da9ac3bdc87d590a754c0e
Sagar Pyakurel <pyakurelsagar35@gmail.com>
2023-08-26 12:01:48 +1000

    login verification complete

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { insertUser } from "../models/user/UserModel.js";
-import { hashPassword } from "../utils/bcrypt.js";
+import { getUserByEmail, insertUser } from "../models/user/UserModel.js";
+import { comparePassword, hashPassword } from "../utils/bcrypt.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -42,6 +42,34 @@ router.post("/", async (req, res) => {
     res.json({
       status: "error",
       message: msg,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    // get the data
+    console.log(req.body);
+
+    const { email, password } = req.body;
+    // check if the user exit with received email and get user from db
+
+    const user = await getUserByEmail(email);
+    if (user?._id) {
+      // use bcrypt to check if password is matching
+
+      const isMatch = comparePassword(password, user.password);
+      console.log(isMatch);
+    }
+
+    res.json({
+      status: "success",
+      message: "login successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
     });
   }
 });
